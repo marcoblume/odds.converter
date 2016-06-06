ced4b1db623486eb3287914a42f8bd8de2f56008
Marco Blume <marcob@pinnaclesports.com>
2016-06-06 09:56:09 -0400

    no message

--- R/odds.dec2us.R
+++ R/odds.dec2us.R
@@ -7,5 +7,6 @@
 #' @examples
 #' odds.dec2us(c(1.93,2.05))
 odds.dec2us <- function (x) {
-        ifelse (x <= 1,NA,ifelse(x >2, 100 * (x - 1),-100/(x - 1)))
+        ifelse (x <= 1,NA,
+                ifelse(x >2, 100 * (x - 1),-100/(x - 1)))
 }
